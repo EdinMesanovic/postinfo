568bae7a0cf6b2ef37f2ce34b2a929b4e6cffad0
EdinMesanovic <edo_tz_@hotmail.com>
2025-10-15 14:53:51 +0200

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,31 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", 'https://bingopostapi.edinmesan.ba'] }));
+const allowedOrigins = ["http://localhost:5173", 'https://bingopostapi.edinmesan.ba'];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (typeof origin === "string" && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  }
+  
+  // Ako je OPTIONS request (preflight request), odmah vraćamo status 200
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev")); // ako želiš logove
 
