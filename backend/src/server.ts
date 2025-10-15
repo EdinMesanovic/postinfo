6303829e0de0a354a4c225f958509fabb8bafdac
EdinMesanovic <edo_tz_@hotmail.com>
2025-10-15 14:50:24 +0200

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ["http://localhost:5173", 'https://bingopostapi.edinmesan.ba'] }));
 app.use(express.json());
 app.use(morgan("dev")); // ako želiš logove
 
